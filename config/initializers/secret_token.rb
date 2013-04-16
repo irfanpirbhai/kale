1e7f322814ee09d2ee1f7f9aa827fa8714e77970
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-04-16 09:07:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kale::Application.config.secret_token = '8bda9876e88cf623375758f7a63ba3a445222d230bee866167d2abd126d7f617124d9093d4d64014f06ebd8f6edef17dddb0a1e67063abfbadda36fa21919823'
